e2492cb18e7d975354a5c0162df87e86b3689a51
pranav <you@example.com>
2021-09-29 18:29:30 -0500

    Init Commit Loop Exercise

--- src/com/perscholas/java_basics/Main.java
+++ src/com/perscholas/java_basics/Main.java
@@ -76,7 +76,77 @@ public class Main {
                 }
             }
         }
+      /*  Write a program that uses nested for-loops to output the following:Week 1:
+Day 1
+Day 2
+Day 3
+Day 4
+Day 5
 */
+/*
+        int weeks = 2, days_in_week = 5;
+
+        for (int i = 1; i <= weeks; ++i) {
+
+            System.out.println("Week: " + i);
+
+            for (int j = 1; j <= days_in_week; ++j) {
+
+                System.out.println("Day: " + j);
+            }
+        }*/
+       /* Write a program that uses a for-loop to loop through the numbers 1-100. Print out each number if is a multiple of 5,
+                but do not print out any numbers greater than 50. Use the “break” keyword to accomplish this.
+*/
+     /*   int max = 100;
+
+        for(int i =1; i <= max; i++)
+        {
+            if(i % 5 == 0)
+            {
+                System.out.println(i);
+            }
+            else if (i > 50)
+                break;
+        }*/
+
+/*        Write a program that uses a for-loop to loop through the numbers 1-100. Print out each number if is a multiple of 5,
+                but do not print out any numbers between 25 and 75. Use the “continue” statement to accomplish this.*/
+        /*int max = 100;
+
+        for(int i =1; i <= max; i++)
+        {
+            if(i % 5 == 0)
+            {
+                if (i > 25 && i < 75)
+                    continue;
+                System.out.println(i);
+            }
+
+        }*/
+        //Write a program that uses a do-while-loop to loop through the numbers 1-10 and prints out each number.
+/*
+        int i=1;
+        do {
+            System.out.println(i);
+            i++;
+        }while (i <=10);*/
+
+      //Write a program that uses a while loop to loop through the numbers 0-100 in increments of 10 and prints out each number.
+
+       /* int i =0;
+        while (i <=100)
+        {
+            System.out.println(i);
+            i = i + 10;
+        }*/
+
+        /*//Write a program that uses a for-loop to loop through the numbers 1-10 and prints out each number.
+
+        for(int i =1; i <=10; i++)
+        {
+            System.out.println(i);
+        }*/
     }//end main
 }//end class
 
